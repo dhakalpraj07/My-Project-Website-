90cc1e3d6689fae83cbfb52d98f088324a6959e4
@Prajwol <125108020+dhakalpraj07@users.noreply.github.com>
2025-01-27 20:54:23 +1100

    Delete MY PROJECT directory

--- MY PROJECT/app.js
+++
@@ -1,7 +0,0 @@
-let cart = [];
-
-function addToCart(itemName, itemPrice) {
-  cart.push({ name: itemName, price: itemPrice });
-  console.log('Added to cart:', itemName, itemPrice);
-  console.log('Current cart:', cart);
-}
